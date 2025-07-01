8c6f2f105d26825613be2869ff829941ed8d61a9
cuongttn2 <caocuongttn2@gmail.com>
2025-07-01 17:09:30 +0700

    impl android document

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.dokka)
 }
 
 android {
@@ -39,7 +40,7 @@ android {
 }
 
 dependencies {
-
+    dokkaPlugin(libs.android.documentation.plugin)
     implementation(libs.kavehcolorpicker)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
