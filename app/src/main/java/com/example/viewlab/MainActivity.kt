6827c0c7e333b119e54d53de96049f57e1a76134
cuongttn2 <caocuongttn2@gmail.com>
2025-07-04 16:13:11 +0700

    research ColorPicker 2

--- app/src/main/java/com/example/viewlab/MainActivity.kt
+++ app/src/main/java/com/example/viewlab/MainActivity.kt
@@ -7,10 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.viewlab.databinding.ActivityMain2Binding
 import com.example.viewlab.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMain2Binding
 
     private var _alpha = 0f
     private var _color = 0
@@ -20,7 +21,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
